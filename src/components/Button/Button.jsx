5e9468706c152a60ae37473f3a946456fe4e6de0
Shovgu <114073036+Shovgu@users.noreply.github.com>
2022-11-23 23:04:48 +0400

    Add files via upload

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,13 +2,19 @@ import React,{Component} from 'react'
 import './Button.css'
 
 class Button extends Component{
-    handleClick(){
+    state={
+        text:"Click here",
+        className:"buton-red"
+    }
+    handleClick=()=>{
         console.log("Button clicked!")
+        this.setState({text:this.state.text==="Click here"?"Clicked":"Click here",className:this.state.className==="buton-red"?"buton-green":"buton-red"})
     }
     render(){
+        const { text,className } = this.state;
         return(
             <div className="btn">
-                <button onClick={this.handleClick}>Click</button>
+                <button className={className} onClick={this.handleClick}>{text}</button>
             </div>
         )
     }
